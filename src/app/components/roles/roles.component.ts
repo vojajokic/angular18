7cc872d4c0d8f6924122abe23e08dafadaf72c2d
vojajokic <55879745+vojajokic@users.noreply.github.com>
2024-11-06 14:37:40 +0000

    Event Binding

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -11,14 +11,17 @@ import { FormsModule } from '@angular/forms';
 export class RolesComponent {
   firstName: string = "Angular Tutorial";
   angularVersion = "Version 18";
-
   version: number = 18;
-
   isActive: boolean = false;
-
   currentDate: Date = new Date();
-
   inputType: string = "button";
-
   selectedState: string = "";
+
+  showWelcomeAlert () {
+    alert("Welcome to Angular 18 Tutorial");
+  }
+
+  showMessage(message: string) {
+    alert(message);
+  }
 }
